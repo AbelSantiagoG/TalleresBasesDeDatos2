22c2a24d14ddc032517aaa145b4c0ca235f5b027
AbelSantiagoG <abelsantiago1000@gmail.com>
2024-08-30 02:14:25 -0500

    Taller 1 arreglado en base al taller 3

--- Taller 2.sql
+++ Taller 2.sql
@@ -39,37 +39,29 @@ BEGIN;
 	VALUES ('789EF', 'Arroz', 50, 5000);
 
 	INSERT INTO pedidos (id, fecha, cantidad, valor_total, id_cliente, id_producto) 
-	VALUES ('2024-08-05', 2, 3000, '1091354977', '123AB');
+	VALUES ('1','2024-08-05', 2, 3000, '1091354977', '123AB');
 	INSERT INTO pedidos (id, fecha, cantidad, valor_total, id_cliente, id_producto) 
-	VALUES ('2024-05-03', 1, 2500, '60354761', '456CD');
+	VALUES ('2','2024-05-03', 1, 2500, '60354761', '456CD');
 	INSERT INTO pedidos (id, fecha, cantidad, valor_total, id_cliente, id_producto) 
-	VALUES ('2024-09-30', 5, 25000, '1091354977', '789EF');
+	VALUES ('3','2024-09-30', 5, 25000, '1091354977', '789EF');
 
-	UPDATE clientes
-	SET nombre = 'Pedro Flores'
-	WHERE identificacion = '1091354977';
-	UPDATE clientes
-	SET edad = 40
-	WHERE identificacion = '60354761';
+	UPDATE clientes SET nombre = 'Pedro Flores' WHERE identificacion = '1091354977';
+	UPDATE clientes SET edad = 40 WHERE identificacion = '60354761';
 
-	UPDATE productos
-	SET valor_unitario = 2400
-	WHERE codigo = '456CD';
-	UPDATE productos
-	SET nombre = 'papaya'
-	WHERE codigo = '456CD';
+	UPDATE productos SET valor_unitario = 2400 WHERE codigo = '456CD'; 
+	UPDATE productos SET nombre = 'papaya' WHERE codigo = '456CD';
 
-	UPDATE pedidos
-	SET cantidad = 15
-	WHERE id = '1234';
-	UPDATE pedidos
-	SET cantidad = 1
-	WHERE id = '4321';
+	UPDATE pedidos SET cantidad = 15 WHERE id = '1';
+	UPDATE pedidos SET cantidad = 1 WHERE id = '2';
 
+	DELETE FROM pedidos where id= '1';
+	
 	DELETE FROM clientes where identificacion= '123456789';
 	
 	DELETE FROM productos where codigo= '123AB';
 	
-	DELETE FROM pedidos where id= '1234';
-	
 COMMIT;
+
+	select * from clientes;
+	select * from productos;
+	select * from pedidos;
