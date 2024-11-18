930e011eebd5a44f23d14e81486c667937c6e703
AbelSantiagoG <abelsantiago1000@gmail.com>
2024-11-18 12:53:30 -0500

    Taller 19 completo

--- Taller19/src/main/java/com/mycompany/taller19/Taller19.java
+++ Taller19/src/main/java/com/mycompany/taller19/Taller19.java
@@ -7,9 +7,15 @@ package com.mycompany.taller19;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gt;
 import static com.mongodb.client.model.Updates.set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.bson.Document;
 
 /**
@@ -22,41 +28,58 @@ public class Taller19 {
         String uri= "mongodb://localhost:27017";
         MongoClient mongoClient= MongoClients.create(uri);
         MongoDatabase database = mongoClient.getDatabase("pruebas");
-        MongoCollection<Document> collection = database.getCollection("usuarios");
+        MongoCollection<Document> collection = database.getCollection("productos");
         System.out.println("Conexion exitosa");
         
-        //Insertar un documento
-        //Document documento = new Document("nombre", "PENE").append("edad", 19).append("ciudad", "Ocaña");
-        //collection.insertOne(documento);
-        
-        //Insertando múltiples documentos
-        //Document documento1= new Document("nombre", "Luisa").append("edad", 29).append("ciudad", "Valencia");
-        //Document documento2= new Document("nombre", "Geovanny").append("edad", 15).append("ciudad", "Yobani");
-        //collection.insertMany(Array.asList(documento1, documento2));
-        
-        //Consultar documentos
-//        MongoCursor<Document> cursor = collection.find().iterator();
-//        while(cursor.hasNext()){
-//            System.out.println(cursor.next().toJson());
-//        }
-        
-        //Eliminar un documento
-        collection.deleteOne(eq("nombre", "PENE"));
+        //Insertar 10 registros
+        List<Document> productos = new ArrayList<>();
 
-        //Traer un solo documento
-        //Document resultado = collection.find(eq("nombre", "PENE")).first();
+        for (int i = 1; i <= 10; i++) {
+            Document producto = new Document("ProductoID", i)
+                    .append("Nombre", "Producto" + i)
+                    .append("Descripción", "Descripción del producto " + i)
+                    .append("Precio", 10 * i)
+                    .append("Categoría", new Document("CategoríaID", i)
+                            .append("NombreCategoria", "Categoría " + i))
+                    .append("Comentarios", Arrays.asList(
+                            new Document("ComentarioID", 1)
+                                    .append("Texto", "Comentario1  " + i)
+                                    .append("Cliente", "Cliente" + i),
+                            new Document("ComentarioID", 2)
+                                    .append("Texto", "Comentario2  " + i)
+                                    .append("Cliente", "Cliente" + (i + 1))
+                    ));
 
-        //Consulta de documentos con filtros
-        //MongoCursor<Document> cursor = collection.find(and(eq("ciudad", "Ocaña"), gt("edad", 19), lt("edad", 10))).limit(5).iterator();
-        //while(cursor.hasNext()){
-        //    System.out.println(cursor.next().toJson());
-        //}
+            productos.add(producto);
+        }
+
+        collection.insertMany(productos);
         
-        //Actualizar un documento
-        collection.updateOne(eq("nombre","PENE"), set("edad", 45));
+        //Actualizar 5 registros
+        for (int i = 1; i <= 5; i++) {
+            collection.updateOne(
+                    eq("ProductoID", i), 
+                    new Document("$set", new Document("Precio", 15 * i)) 
+            );
+        }   
         
+        // Eliminar 2 registros
+        collection.deleteOne(eq("ProductoID", 1));
+        collection.deleteOne(eq("ProductoID", 2));
+ 
+        //Productos con precio mayor a 10 dólares
+        MongoCursor<Document> cursor = collection.find(gt("Precio", 10)).iterator();
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next().toJson());
+        }
         
-        
-     
+        //Productos con precio mayor a 50 dólares y categoría igual a ropa
+        MongoCursor<Document> cursor2 = collection.find(
+        and(gt("Precio", 50), eq("Categoría.NombreCategoria", "Ropa"))
+        ).iterator();
+
+        while (cursor2.hasNext()) {
+            System.out.println(cursor2.next().toJson());
+        }
     }
 }
